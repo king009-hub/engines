4f34e5acde8ff18ab9e079e2954dec6388d18012
king009 <erikdriver2025@gmail.com>
2026-04-12 21:11:00 +0200

    Fix Stripe mutable prop and clean up package.json

---
+++ assets/label-DGxWE-tt.js
@@ -0,0 +1,1 @@
+import{r,j as t}from"./query-vendor-B5jppBZd.js";import{W as a}from"./ui-vendor-Cu27-ej5.js";import{c as m,b as i}from"./index-vfqz3297.js";const l=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:m(l(),s),...e}));p.displayName=a.displayName;export{p as L};
