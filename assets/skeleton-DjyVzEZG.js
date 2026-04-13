464df0862756d5b8a3fde4d0e13815c7d824edf2
king009 <erikdriver2025@gmail.com>
2026-04-13 08:48:02 +0200

    Optimize checkout speed: parallelized data fetching and server-side total calculation

---
+++ assets/skeleton-DjyVzEZG.js
@@ -0,0 +1,1 @@
+import{j as m}from"./query-vendor-B5jppBZd.js";import{c as o}from"./index-C6sS9LGt.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
