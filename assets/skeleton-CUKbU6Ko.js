8354f5e58fe087a5b6d33e292a52489bcf27a87e
king009 <erikdriver2025@gmail.com>
2026-05-02 15:30:58 +0200

    Fix product load issue and rebuild

--- assets/skeleton-CUKbU6Ko.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./vendor-utils-CCResyfZ.js";import{c as o}from"./index-D85CcIqD.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
